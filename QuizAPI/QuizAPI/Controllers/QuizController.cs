95408c814d14c8eeb5859929af6c1c00162b4964
Blackbeard-Teach <keamogetswemfoloe@gmail.com>
2021-06-10 12:11:54 +0200

    Project Structure

---
+++ QuizAPI/QuizAPI/Controllers/QuizController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace QuizAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class QuizController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult GetQuiz(string quizType)
+        {
+            return Ok();
+        }
+
+        [HttpPost]
+        public ActionResult PostQuestion(string question, string category, string answer, List<string>choices)
+        {
+            return Ok();
+        }
+
+        [HttpDelete]
+        public ActionResult deleteQ(int id)
+        {
+            return Ok();
+        }
+    }
+}
---
+++ QuizAPI/QuizAPI/Models/Quiz.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace QuizAPI.Models
+{
+    public class Question
+    {
+        public int QuestionID { get; set; }
+        public string QuestionText { get; set; }
+        public string QuestionType { get; set; }
+        public string Answer { get; set; }
+        public ICollection<Choice> Choices { get; set; }
+        public int CategoryID { get; set; }
+    }
+
+    public class Category
+    {
+        public int CategoryID { get; set; }
+        public string CategoryText { get; set; }
+    }
+
+    public class Answer
+    {
+        public int QuestionID { get; set; }
+        public string QuestionText { get; set; }
+        public bool IsCorrect { get; set; }
+
+    }
+
+    public class Choice
+    { }
+
+    public class Quiz
+    {
+        public int QuizID { get; set; }
+        public string QuizType { get; set; } //MCQ OR True/False
+        public string CategoryID { get; set; }
+        public string Name { get; set; }
+    }
+}
